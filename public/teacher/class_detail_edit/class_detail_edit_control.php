a88b6fec898f9fa62966934ced2e24c5fbcce739
icc24004 <icc24004@isahaya-cc.ac.jp>
2026-01-08 13:34:22 +0900

    リンク情報の変更処理

---
+++ public/teacher/class_detail_edit/class_detail_edit_control.php
@@ -0,0 +1,14 @@
+<?php
+// teacher_home_control.php
+// ブラウザがアクセスする際のコントローラーを呼び出すファイル
+// セッション開始とログイン判定を一括で行う
+
+// 1. セキュリティ設定
+require_once '../../../app/classes/security/SecurityHelper.php';
+SecurityHelper::applySecureHeaders();
+
+// 2. コントローラーとヘルパーの読み込み
+require_once '../../../app/controllers/teacher/class_detail_edit/ClassDetailEditersController.php';
+
+$controller = new ClassDetailEditersController();
+$controller->index();
