7897813829fae44306e4a01fb8fa9323add71c4a
icc24033 <icc24033@isahaya-cc.ac.jp>
2026-01-27 21:21:20 +0900

    エラー表示のコード削除

--- public/master/class_subject_edit/controls/addition_control.php
+++ public/master/class_subject_edit/controls/addition_control.php
@@ -1,10 +1,5 @@
 <?php
-// addition_control.php
-
-// --- デバッグ用：エラーを表示させる設定（解決したら削除してください） ---
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// addition_control.
 
 require_once '../../../../app/classes/security/SecurityHelper.php';
 SecurityHelper::applySecureHeaders();
