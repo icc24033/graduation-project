e5dfb0fe636574eb3ef72175b8469677aed7d2cc
icc24033 <icc24033@isahaya-cc.ac.jp>
2026-01-28 09:42:43 +0900

    先生アカウント編集 コード成型

--- public/master/teacher_account_edit/master.php
+++ public/master/teacher_account_edit/master.php
@@ -2,9 +2,6 @@
 // require_once __DIR__ . '/../session/session_config.php'; // セッション設定を読み込む
 
 // SecurityHelperの読み込み
-require_once __DIR__ . '/../../../app/classes/security/SecurityHelper.php';
-SecurityHelper::applySecureHeaders();
-
 ?>
 
 
