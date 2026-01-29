935dff8000f3a24515d7f0761b5ab51ca0b922db
icc24033 <icc24033@isahaya-cc.ac.jp>
2026-01-29 11:59:32 +0900

    target="_blank"の実装

--- public/master/class_subject_edit/tuika.php
+++ public/master/class_subject_edit/tuika.php
@@ -19,7 +19,7 @@
                     <span class="icon-key"></span>
                         アプリからログアウト
                 </a>
-                <a href="../../../help/help_control.php?back_page=4" class="help-button">
+                <a href="../../../help/help_control.php?back_page=4" class="help-button" target="_blank" rel="noopener noreferrer">
                     <span class="icon-lightbulb"></span> ヘルプ
                 </a>
             </div>
