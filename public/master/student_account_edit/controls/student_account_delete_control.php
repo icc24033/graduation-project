97db59153f922a3ddd7053c83ef27180bfc101e9
icc24033 <icc24033@isahaya-cc.ac.jp>
2026-01-29 11:20:56 +0900

    セキュリティヘッダーの適用

--- public/master/student_account_edit/controls/student_account_delete_control.php
+++ public/master/student_account_edit/controls/student_account_delete_control.php
@@ -2,7 +2,15 @@
 require_once __DIR__ . '/../../../../app/session/session_config.php'; // セッション設定を読み込む
 
 require_once '../../../../app/classes/security/SecurityHelper.php';
+// セキュリティヘッダーの適用
 SecurityHelper::applySecureHeaders();
+
+// セッション開始とログイン判定を一括で行う
+SecurityHelper::requireLogin();
+
+// セキュリティヘッダーを適用
+SecurityHelper::applySecureHeaders();
+
 if (session_status() === PHP_SESSION_NONE) session_start();
 
 require_once '../../../../app/controllers/master/student_account_editers/StudentAccountEditController.php';
