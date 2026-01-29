edc6c01a9e2c2ed879ba6942006b97ef820e57f3
icc24004 <icc24004@isahaya-cc.ac.jp>
2026-01-29 14:14:31 +0900

    aaa

--- public/teacher/class_detail_edit/class_detail_edit.php
+++ public/teacher/class_detail_edit/class_detail_edit.php
@@ -186,19 +186,19 @@ $subjectList = $subjectList ?? [];
         </script>
         <script>
             document.addEventListener('DOMContentLoaded', function() {
-                    const userAvatar = document.getElementById('userAvatar');
-                    const userMenuPopup = document.getElementById('userMenuPopup');
+                const userAvatar = document.getElementById('userAvatar');
+                const userMenuPopup = document.getElementById('userMenuPopup');
 
-                    userAvatar.addEventListener('click', function(event) {
-                        userMenuPopup.classList.toggle('is-visible');
-                        event.stopPropagation();
-                    });
+                userAvatar.addEventListener('click', function(event) {
+                    userMenuPopup.classList.toggle('is-visible');
+                    event.stopPropagation();
+                });
 
-                    document.addEventListener('click', function(event) {
-                        if (!userMenuPopup.contains(event.target) && !userAvatar.contains(event.target)) {
-                            userMenuPopup.classList.remove('is-visible');
-                        }
-                    });
+                document.addEventListener('click', function(event) {
+                    if (!userMenuPopup.contains(event.target) && !userAvatar.contains(event.target)) {
+                        userMenuPopup.classList.remove('is-visible');
+                    }
+                });
             });
         </script>
         <script src="js/class_detail_edit.js"></script>
