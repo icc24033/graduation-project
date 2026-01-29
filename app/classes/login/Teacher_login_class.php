5c481cf04d223c772751bfc34a1faa0a36397d16
icc24033 <icc24033@isahaya-cc.ac.jp>
2026-01-29 11:08:32 +0900

    生徒アカウントに対する自動処理の修正

--- app/classes/login/Teacher_login_class.php
+++ app/classes/login/Teacher_login_class.php
@@ -13,9 +13,9 @@ class TeacherLogin implements LoginUser {
         $this->gradeName = $gradeName;
 
         $month = date('n');
-        if ($month === '1') {
+        if ($month === '4') {
             // 4月は進級処理
-            require_once __DIR__ . '/../../functions/master/grade_increment.php';
+            require_once __DIR__ . '/../../functions/master/grade_increment.php';   // こっちはできた
         } elseif ($month === '5') {
             // 5月は卒業生削除処理
             require_once __DIR__ . '/../../functions/master/graduate_delete.php';
